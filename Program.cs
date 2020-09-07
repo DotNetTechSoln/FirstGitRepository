beea32252e203f68954ac9accbe276a78290c1a8
jayashree <55239135+DotNetTechSoln@users.noreply.github.com>
2020-09-07 19:08:00 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            rectangle rectangle = new rectangle(23.21,23.23);
+            rectangle.getArea();
+            circle circle = new circle(23.12);
+            circle.getArea();
+            Console.WriteLine("area of rectangle is" + rectangle.getArea());
+            Console.WriteLine("area of circle is" + circle.getArea());
+            Console.ReadLine();
+           
+        }
+    }
+}
